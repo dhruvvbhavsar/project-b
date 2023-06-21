75892065a1bc91a7efa4ea18f9705d6c6b553f68
Dhruv Bhavsar <dhruv.bhavsar75@gmail.com>
2023-06-21 13:46:39 +0530

    demo_1

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
-  if (!req.cookies.has("id")) {
-    return NextResponse.redirect(new URL("/welcome", req.url));
-  } else {
+  if (req.cookies.has("id")) {
     return NextResponse.next();
+  } else if(!req.cookies.has("id")) {
+    return NextResponse.redirect(new URL("/welcome", req.url));
   }
 }
 
 export const config = {
-  matcher: ["/dashboard", "/"],
+  matcher: ["/dashboard"],
 };
