01a26b21bd8b97c1d379ac4025614b9994163726
Dhruv Bhavsar <dhruv.bhavsar75@gmail.com>
2023-06-30 22:20:11 +0530

    landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-import Link from "next/link";
-export default function Home() {
-  return (
-    <>
-      <main className="h-screen w-full bg-black text-white flex flex-col justify-center items-center">
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          Landing page here
-        </h1>
-      </main>
-    </>
-  );
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Redirect() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/landing-page/index.html");
+  });
+
+  return null; 
 }
