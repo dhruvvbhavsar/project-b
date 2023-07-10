366927db7db54a2596cbcbfd3c8d392c30998f84
Dhruv Bhavsar <dhruv.bhavsar75@gmail.com>
2023-07-10 18:44:45 +0530

    upload images

--- storage/atoms.js
+++ storage/atoms.js
@@ -3,9 +3,11 @@ import { atom } from 'jotai';
 const sliceStartAtom = atom(0)
 const sliceEndAtom = atom(5)
 const currentPageAtom = atom(1)
+const fileAtom = atom(null || 'string');
 
 export {
     sliceStartAtom,
     sliceEndAtom,
-    currentPageAtom
+    currentPageAtom,
+    fileAtom
 };
